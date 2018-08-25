8a0ccad4c648714156c250188dbf94fa322e5826
Ngari Ndung'u <ngari.ndungu@gmail.com>
2018-08-25 20:04:40 +0300

    maintain background on orientation change

--- app/src/main/java/com/createchs/middleearth/MainActivity.java
+++ app/src/main/java/com/createchs/middleearth/MainActivity.java
@@ -1,8 +1,10 @@
 package com.createchs.middleearth;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -11,20 +13,34 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    // list to keep track of whether questions are correctly answered
+    private boolean[] correctlyAnswered = new boolean[7];
+    private static final String ANSWERS = "answers";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // attach click listener to submit button
+        Button submitButton = (Button) findViewById(R.id.submit_button);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getScores();
+            }
+        });
     }
 
     /*
-    * Method for calculating the score, called on submit
-    */
-    public void getScores(View view) {
+     * Method for calculating the score, called on submit
+     */
+    public void getScores() {
         int score = 0;
         /*
-         Multi-choice multi answer questions
-        */
+         * Multi-choice multi answer questions
+         * Scored by checking the choices with their expected checked state
+         */
         int[] questionFiveChoices = {
                 R.id.question5_option1,
                 R.id.question5_option2,
@@ -32,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
                 R.id.question5_option4
         };
         boolean[] qFiveExpectedState = {false, true, false, true};
+
         int[] questionSixChoices = {
                 R.id.question6_option1,
                 R.id.question6_option2,
@@ -40,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
         };
         boolean[] qSixExpectedState = {false, true, true, false};
 
+        // Answers to all questions
         Object[][] answers = {
                 {R.id.question1_option1},
                 {R.id.question2_option5},
@@ -49,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
                 {questionSixChoices, qSixExpectedState},
                 {0}
         };
+
         String[] viewTypes = {
                 "RadioButton",
                 "RadioButton",
@@ -58,8 +77,8 @@ public class MainActivity extends AppCompatActivity {
                 "CheckBox",
                 "EditText"
         };
-        boolean[] correctlyAnswered = new boolean[7];
 
+        // text inputs
         int[] editViews = {
                 R.id.question3_ans,
                 R.id.question4_ans,
@@ -75,8 +94,8 @@ public class MainActivity extends AppCompatActivity {
                     }
                     break;
                 case "EditText":
-                    if (((EditText) findViewById(editViews[editViewsIndex])).getText().toString().equals(
-                            String.valueOf(answers[i][0])
+                    if (((EditText) findViewById(editViews[editViewsIndex])).getText().toString().
+                            equals(String.valueOf(answers[i][0])
                     )) {
                         score += 1;
                         correctlyAnswered[i] = true;
@@ -84,7 +103,8 @@ public class MainActivity extends AppCompatActivity {
                     }
                     break;
                 case "CheckBox":
-                    boolean isCorrect = checkMultiChoiceQuestion(((int[]) answers[i][0]), ((boolean[]) answers[i][1]));
+                    boolean isCorrect = checkMultiChoiceQuestion(((int[]) answers[i][0]),
+                            ((boolean[]) answers[i][1]));
                     if (isCorrect) {
                         score += 2;
                         correctlyAnswered[i] = isCorrect;
@@ -93,12 +113,12 @@ public class MainActivity extends AppCompatActivity {
             }
 
         }
-        giveFeedback(score, correctlyAnswered);
+        giveFeedback(score, null);
     }
 
     /*
-    * This method checks that only the correct answers are checked for multiple choice questions
-    */
+     * This method checks that only the correct answers are checked for multiple choice questions
+     */
     private boolean checkMultiChoiceQuestion(int[] questionChoices, boolean[] qExpectedState) {
         if (questionChoices.length == qExpectedState.length) {
             for (int i = 0; i < questionChoices.length; i++) {
@@ -111,10 +131,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /*
-    * Method provides feedback to the user, by displaying the score and setting the background
-    * on question cards depending on whether the question is correctly answered
-    */
-    private void giveFeedback(int score, boolean[] correctlyAnswered) {
+     * Method provides feedback to the user, by displaying the score and setting the background
+     * on question cards depending on whether the question is correctly answered
+     */
+    private void giveFeedback(int score, @Nullable Bundle savedInstanceState) {
         int[] questionCardViews = {
                 R.id.question1_card,
                 R.id.question2_card,
@@ -130,22 +150,37 @@ public class MainActivity extends AppCompatActivity {
                         getResources().getColor(R.color.incorrect_color, null));
             } else {
                 findViewById(questionCardViews[i]).setBackgroundColor(
-                        getResources().getColor(R.color.colorSecondaryDark,null)
+                        getResources().getColor(R.color.colorSecondaryDark, null)
                 );
             }
         }
 
-        String message;
-        if (score < 5) {
-            message = getResources().getString(R.string.poor);
-        } else if (score < 7) {
-            message = getResources().getString(R.string.average);
-        } else {
-            message = getResources().getString(R.string.good);
+        if (savedInstanceState == null) {
+            String message;
+            if (score < 5) {
+                message = getResources().getString(R.string.poor);
+            } else if (score < 7) {
+                message = getResources().getString(R.string.average);
+            } else {
+                message = getResources().getString(R.string.good);
+            }
+
+            String toastMessage = String.format("You scored %s/9. %s", score, message);
+
+            Toast.makeText(this, toastMessage, Toast.LENGTH_LONG).show();
         }
+    }
 
-        String toastMessage = String.format("You scored %s/9. %s",score,message);
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putBooleanArray(ANSWERS, correctlyAnswered);
+        super.onSaveInstanceState(outState);
+    }
 
-        Toast.makeText(this, toastMessage, Toast.LENGTH_LONG).show();
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        correctlyAnswered = savedInstanceState.getBooleanArray(ANSWERS);
+        giveFeedback(0,savedInstanceState);
     }
 }
